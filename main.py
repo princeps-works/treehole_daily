c11c56aafbbfeade0428878dc26d1d3b2749e516
princeps-works <121336207+princeps-works@users.noreply.github.com>
2023-09-21 23:49:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,12 @@ import logging
 import os
 from world_cloud import texts_2_word_cloud, get_text
 from integration.lark import send
-
+import get_words
 
 def main():
     today = datetime.date.today()
     delta = datetime.timedelta(days=-1)
     yesterday = today + delta
-
-    sql = f"select content from fduhole.floor where updated_at between '{yesterday}' and '{today}' and deleted = 0"
-    text = get_text(os.environ.get("DB_URL"), sql) 
-    wc = texts_2_word_cloud(text)
-    wc.to_file(f"./data/output/{yesterday}/wordscloud.png")
     newspaper = 
     with open(f"./data/output/{yesterday}.png", "rb") as f:
         send(f)
