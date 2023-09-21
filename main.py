85f03db9cf4b135a179b8936804049c306e14ac3
princeps-works <121336207+princeps-works@users.noreply.github.com>
2023-09-21 23:37:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,16 +10,11 @@ def main():
     delta = datetime.timedelta(days=-1)
     yesterday = today + delta
 
-    sql1 = f"select content from fduhole.floor where updated_at between '{yesterday}' and '{today}' and deleted = 0"
-    sql2 = f"select content,like,hole_id from fduhole.floor where created_at between '{yesterday}' and '{today}' and deleted = 0"
-    text1 = get_text(os.environ.get("DB_URL"), sql1) 
-    text2 = get_text(os.environ.get("DB_URL"), sql2) 
-    wc，command = texts_2_word_cloud(text1)
-    text_command = ''
-    for wds in command:
-        text_command += '
-    wc.to_file(f"./data/output/{yesterday}.png")
-    
+    sql = f"select content from fduhole.floor where updated_at between '{yesterday}' and '{today}' and deleted = 0"
+    text = get_text(os.environ.get("DB_URL"), sql) 
+    wc = texts_2_word_cloud(text)
+    wc.to_file(f"./data/output/{yesterday}/wordscloud.png")
+    newspaper = 
     with open(f"./data/output/{yesterday}.png", "rb") as f:
         send(f)
 
